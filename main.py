72ccb17d0d5541bde96c30fa3729f91edef6c2cd
Anik <raiyan.csebuet@gmail.com>
2023-09-17 03:05:55 +0600

    database updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from clean import formatDateDayIndex
+import pandas as pd
+def cleanDate(fromY, toY):
+    for y in range(fromY, toY+1):
+        formatDateDayIndex(year=y)
 
-for y in range(2010, 2023):
-    formatDateDayIndex(year=y)
- 
-# Get the list of all files and directories
+df = pd.read_csv("database/clean-data/DSE-2022.csv")
+print(df.Scrip.unique())
